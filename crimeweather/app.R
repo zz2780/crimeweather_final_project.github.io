ccfbaaefd88929a072eba53d48bd146d71afeea4
zz2780 <zz2780@cumc.columbia.edu>
2022-12-07 12:25:17 -0500

    commit--method in index updated, report updated with motivation, initial q, and partial exploratory analysis

---
+++ crimeweather/app.R
@@ -0,0 +1,68 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(flexdashboard)
+library(ggplot2)
+library(readr)
+library(tidyverse)
+library(plotly)
+
+crime_df <- read.csv("data/crime_df.csv")%>%
+  mutate(
+    season=case_when(
+      mon %in% c(6,7,8)~"Summer",
+      mon %in% c(9,10,11)~"Fall",
+      mon %in% c(12,1,2)~"Winter",
+      mon %in% c(3,4,5)~"Spring",
+    )
+  )
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+      boros = crime_df %>% distinct(borough) %>% pull()
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
